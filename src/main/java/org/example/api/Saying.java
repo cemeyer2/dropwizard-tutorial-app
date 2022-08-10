ef6affa5b89d89d1c82f4c926f2f99e6e26a7ccf
Charlie Meyer <charlie.meyer@data.world>
2022-08-10 12:17:05 -0500

    simple app

---
+++ src/main/java/org/example/api/Saying.java
@@ -0,0 +1,26 @@
+package org.example.api;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Saying {
+    private long id;
+
+    private String content;
+
+    public Saying() {}
+
+    public Saying(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    @JsonProperty
+    public long getId() {
+        return this.id;
+    }
+
+    @JsonProperty
+    public String getContent() {
+        return this.content;
+    }
+}
